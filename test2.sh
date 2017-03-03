b6a50e8b402e7957559445faacc46416dbf72d43
Alex Kaplan <kaplan2539@gmail.com>
2017-03-03 15:01:43 -0800

    test2: fail if ntc-docs-$\{CI_BUILD_REF_NAME\}.surge.sh is not online

--- test2.sh
+++ test2.sh
@@ -9,6 +9,7 @@ apt-get install wget
 TMPDIR=$(mktemp -d)
 URL=http://ntc-docs-${CI_BUILD_REF_NAME}.surge.sh
 PAGES=$(wget -q -O - $URL |grep _blank |sed -e 's/.*href=\"\([^"]*\)\".*/\1/g')
+PAGE=${PAGES:?got empty response from $URL}
 
 for P in $PAGES; do
     wget -q -O $TMPDIR/$P $URL/$P || exit $?

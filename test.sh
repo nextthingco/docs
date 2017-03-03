e315cb85425c29b4c045666ba8a077ea3ddad255
Alex Kaplan <kaplan2539@gmail.com>
2017-03-03 15:47:40 -0800

    test.sh: fail if response from http://ntc-docs-${CI_BUILD_REF_NAME}.surge.sh is empty

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+CI_BUILD_REF_NAME=${CI_BUILD_REF_NAME:?cannot be empty}
+
 apt-get install wget
 
 [ -x "$(which wget)" ] || exit 1
@@ -9,6 +11,7 @@ apt-get install wget
 TMPDIR=$(mktemp -d)
 URL=http://ntc-docs-${CI_BUILD_REF_NAME}.surge.sh
 PAGES=$(wget -q -O - $URL |grep _blank |sed -e 's/.*href=\"\([^"]*\)\".*/\1/g')
+PAGE=${PAGES:?got empty response from $URL}
 
 for P in $PAGES; do
     wget -q -O $TMPDIR/$P $URL/$P || exit $?

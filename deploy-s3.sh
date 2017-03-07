3e21f4ed6d32566f700284d3646dd2521816c1ac
Alex Kaplan <kaplan2539@gmail.com>
2017-03-06 17:42:23 -0800

    deploy-s3.sh: moved from bamboo to githost.io variables

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -5,15 +5,20 @@ set -x
 
 #NOTE: no pushd here!
 
-export GIT_DEPLOY_DIR="${bamboo.build.working.directory}/output"
+export OUTPUT_DIR="${PWD}/output"
 
 # login aws cli
-export AWS_ACCESS_KEY_ID=${bamboo.AwsAccessKeyId}
-export AWS_SECRET_ACCESS_KEY=${bamboo.AwsSecretAccessKeyPassword}
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:?not set}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:?not set}
 export AWS_DEFAULT_REGION=us-west-2
 
 # send files
-aws s3 sync ${GIT_DEPLOY_DIR} s3://docs.getchip.com/ --delete --exclude ".git/*" --exclude "*.sh" --exclude "CNAME" --exclude "README.md" --exclude "gulpfile.js" --exclude "package.json" --exclude "src/*"
+aws s3 sync ${OUTPUT_DIR} s3://docs.getchip.com/ --delete --exclude ".git/*" --exclude "*.sh" --exclude "CNAME" --exclude "README.md" --exclude "gulpfile.js" --exclude "package.json" --exclude "src/*"
+
+# let things settle...
+echo "Sleeping for 30 seconds"
+sleep 30
+
 # invalidate cloudfront cache
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id E3BUJIDTZ80HE3 --paths '/*'
